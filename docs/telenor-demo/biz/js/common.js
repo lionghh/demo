53ce326d15111783545867acd6086786f880977c
guohh <guohh@asiainfo.com>
2018-01-24 17:22:44 +0800

    处理ios按钮没有点击效果问题

--- docs/telenor-demo/biz/js/common.js
+++ docs/telenor-demo/biz/js/common.js
@@ -2,4 +2,8 @@ require(['jquery', 'ipu', 'iScroll'], function ($, ipu, iScroll) {
     $(".todo").click(function () {
         ipu.toast('暂未实现');
     });
+	
+	$("body").on("touchstart", function () {
+       // 让ios点击效果生效
+    });
 });
